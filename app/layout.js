5ca310e2907525908a219a7c653d066da042bf96
codingshot <45281667+codingshot@users.noreply.github.com>
2024-05-17 13:06:44 -0400

    made footer flush and the body centered

--- app/layout.js
+++ app/layout.js
@@ -6,8 +6,7 @@ import clsx from "clsx";
 export const metadata = {
   metadataBase: new URL("http://localhost:3000"),
   title: "traction.money",
-  description:
-    "Get money for on-chain traction",
+  description: "Get money for on-chain traction",
   twitter: {
     card: "summary_large_image",
     creator: "@potlock_",
@@ -20,16 +19,17 @@ export default function RootLayout({ children }) {
       <body
         className={clsx(
           GeistSans.className,
-          "w-screen h-screen", // Legacy browser support
-          "w-svw h-svh flex flex-col items-center justify-between"
+          "min-h-screen flex flex-col"
         )}
       >
-        {/* <Navbar /> */}
+        <Navbar />
 
-        <main className="w-full">{children}</main>
+        <main className="flex-grow w-full flex justify-center items-center">
+          {children}
+        </main>
 
         <footer className="py-4 border-t w-full">
-          <div className="wrapper text-center">
+          <div className="max-w-screen-lg mx-auto text-center">
             <p className="text-sm text-black">
               {`With ❤️ by Potluck Labs, Inc. ${new Date().getFullYear()}`}
             </p>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,45 +4,7 @@ import Link from "next/link.js";
 const Navbar = () => {
   return (
     <nav className="bg-transparent border-transparent border-b-2 p-5 relative transition-all items-center flex justify-between container w-full">
-      <Link href="/" target="_blank" className="flex flex-col justify-center text-xl font-bold">
-        <span className="flex flex-nowrap items-end gap-2 pb-1">
-          <Image src={"/icon.png"} height={32} width={32} alt="PotLock logo" />
-          <span className="text-nowrap">traction.money</span>
-        </span>
-      </Link>
 
-      <div className="dropdown dropdown-hover dropdown-end">
-        {/* <div
-          tabIndex={0}
-          role="button"
-          className="btn btn-link text-lg decoration-dotted underline-offset-2"
-        >
-          Products
-        </div> */}
-        <Link href="/learn" target="_blank" className="flex items-end">
-              {/* <Image src={"/nadabot.png"} height={24} width={24} alt="nadabot" /> */}
-              <span>Nadabot</span>
-            </Link>
-
-        <ul
-          tabIndex={0}
-          className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
-        >
-          <li>
-            <Link href="/bot" target="_blank" className="flex items-end">
-              <Image src={"/nadabot.png"} height={24} width={24} alt="nadabot" />
-              <span>Nadabot</span>
-            </Link>
-          </li>
-
-          <li>
-            <Link href="/potlock" target="_blank" className="flex items-end">
-              <Image src={"/icon.png"} height={24} width={24} alt="PotLock logo" />
-              <span>PotLock</span>
-            </Link>
-          </li>
-        </ul>
-      </div>
     </nav>
   );
 };
